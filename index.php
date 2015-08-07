67c7244f22f8dca036cca70af5166892a75f2234
Gary Bunofsky <garybunofsky@gmail.com>
2015-08-07 15:51:59 -0400

    Structure edits.

--- index.php
+++ index.php
@@ -12,33 +12,93 @@
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 	</head>
 	<body>
-		<div class="conatiner-fluid">
-			<div class="photo-area">
-				<div class="col-sm-3">
-					<div class="outer">
-						<div class="banner">
-							<p>Refer a Patient</p>
+		<div class="photo-area">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-3">
+						<div class="outer">
+							<div class="banner">
+								<p>Refer a Patient</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-sm-3">
+						<div class="outer">
+							<div class="banner">
+								<p>Refer a Patient</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-sm-3">
+						<div class="outer">
+							<div class="banner">
+								<p>Refer a Patient</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-sm-3">
+						<div class="form-container">
+							<h4>Contact Admissions</h4>
+							<form>
+								<input class="form-control" type="text" placeholder="Full Name">
+								<input class="form-control" type="text" placeholder="Email Address">
+								<button type="button" class="btn pull-right">Next <i class="fa fa-angle-right">&nbsp;</i></button>
+							</form>
 						</div>
 					</div>
 				</div>
-				<div class="col-sm-3">
-					<div class="outer">
-						<div class="banner">
-							<p>Refer a Patient</p>
+			</div>
+		</div>
+		<div class="list-area">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-9">
+						<div class="row subject">
+							<div class="col-md-3">
+								<img src="img/160-1.jpg" alt="image" />
+							</div>
+							<div class="col-md-9">
+								<h4>Adult Addiction Recovery Program</h4>
+								<p>We understand that out patients come froma  myriad of backgrounds and complicated, painful situations. We provide holistic treatment that affects mind, body, soul and spirit.<br /><br /><a href="#">Learn about adult addiction treatment <i class="fa fa-angle-right">&nbsp;</i></a>
+								</p>
+							</div>
+						</div>
+						<div class="row subject">
+							<div class="col-md-3">
+								<img src="img/160-2.jpg" alt="image" />
+							</div>
+							<div class="col-md-9">
+								<h4>Young Adult Addiction Recovery Program</h4>
+								<p>Young adults who suffer from addiction have more difficulty achieving sobriety than their adult counterparts. Our Young Adult Addiction Program is designed to address this difficulty.<br /><br /><a href="#">Learn about young adult addiction treatment <i class="fa fa-angle-right">&nbsp;</i></a>
+								</p>
+							</div>
+						</div>
+						<div class="row subject">
+							<div class="col-md-3">
+								<img src="img/160-3.jpg" alt="image" />
+							</div>
+							<div class="col-md-9">
+								<h4>Professional Treatment Program</h4>
+								<p>There is hope for professionals dealing with an addiction, which can affect your medical, mental or spiritual well-being. Talbott Recovery is hear and is ready to help.<br /><br /><a href="#">Learn about professionals with addiction treatment <i class="fa fa-angle-right">&nbsp;</i></a>
+								</p>
+							</div>
 						</div>
 					</div>
-				</div>
-				<div class="col-sm-3">
-					<div class="outer">
-						<div class="banner">
-							<p>Refer a Patient</p>
-						</div>
-					</div>
-				</div>
-				<div class="col-sm-3">
-					<div class="outer">
-						<div class="banner">
-							<p>Refer a Patient</p>
+					<div class="col-sm-3">
+						<div class="sidebar">
+							<h4>Community Events</h4>
+							<h6>Georgia Psychiatric Physicians Association Summer Meeting</h6>
+							<p>July 24-25, 2015 – Amelia Island, FL</p>
+							<h6>National Conference on Addiction Disorders Annual Meeting</h6>
+							<p>August 8-11, 2015 – St. Louis, MO</p>
+							<a href="#">View all events <i class="fa fa-angle-right">&nbsp;</i></a>
+							<hr />
+							<h4>Employment &amp; Continuing Education</h4>
+							<ul>
+								<li><a href="#">Current Career Opportunities</a></li>
+								<li><a href="#">Employment Practices</a></li>
+								<li><a href="#">CEU Seminars</a></li>
+							</ul>
 						</div>
 					</div>
 				</div>
@@ -46,61 +106,63 @@
 		</div>
 		<div class="container-fluid">
 			<div class="row quote-area">
-				<div class="col-sm-8 col-sm-offset-2">
+				<div class="col-sm-6 col-sm-offset-3">
 					<p><em>Talbott offered a treatment team who came together to help me pick up the pieces of my life, helped me remember I had a soul..."</em></p>
 					<p><span><em>Female Patient in 2014</em></span><br /><a href="#">Watch the video</a></p>
 				</div>
 			</div>
 		</div>
-		<div class="container-fluid">
-			<div class="row feature-area">
-				<div class="col-sm-3 about">
-					<div class="circle">
-						<i class="fa fa-lemon-o">&nbsp;</i>
+		<div class="feature-area">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-3 about">
+						<div class="circle">
+							<i class="fa fa-lemon-o">&nbsp;</i>
+						</div>
+						<h4>About Us</h4>
+						<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut.<br /><br /><a href="#">Learn more about us <i class="fa fa-angle-right">&nbsp;</i></a><br /></p>
 					</div>
-					<h4>About Us</h4>
-					<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut.<br /><br /><a href="#">Learn more about us <i class="fa fa-angle-right">&nbsp;</i></a><br /></p>
-				</div>
-				<div class="col-sm-3 location">
-					<div class="circle">
-						<i class="fa fa-heart">&nbsp;</i>
+					<div class="col-md-3 location">
+						<div class="circle">
+							<i class="fa fa-heart">&nbsp;</i>
+						</div>
+						<h4>Our Locations</h4>
+						<ul>
+							<li>
+									<a href="#">Talbott Recovery</a><br />
+									5448 Yorktowne Drive, Atlanta, GA 30349
+							</li>
+							<li>
+									<a href="#">Talbott Recovery</a><br />
+									2153 Peachford Road, Dunwoody, GA 30338
+							</li>
+							<li>
+									<a href="#">Talbott Recovery</a><br />
+									1200 Brookstone Centre Parkway,Suite 210 Columbus, OH 31904
+							</li>
+						</ul>
 					</div>
-					<h4>Our Locations</h4>
-					<ul>
-						<li>
-								<a href="#">Talbott Recovery</a><br />
-								5448 Yorktowne Drive, Atlanta, GA 30349
-						</li>
-						<li>
-								<a href="#">Talbott Recovery</a><br />
-								2153 Peachford Road, Dunwoody, GA 30338
-						</li>
-						<li>
-								<a href="#">Talbott Recovery</a><br />
-								1200 Brookstone Centre Parkway,Suite 210 Columbus, OH 31904
-						</li>
-					</ul>
-				</div>
-				<div class="col-sm-3 twitter">
-					<div class="circle">
-						<i class="fa fa-twitter">&nbsp;</i>
+					<div class="col-md-3 twitter">
+						<div class="circle">
+							<i class="fa fa-twitter">&nbsp;</i>
+						</div>
+						<ul>
+							<?php require("twitter.php"); ?>
+						</ul>
 					</div>
-					<ul>
-						<?php require("tweet.php"); ?>
-					</ul>
-				</div>
-				<div class="col-sm-3 photo">
-					<div class="circle">
-						<i class="fa fa-camera">&nbsp;</i>
+					<div class="col-md-3 photo">
+						<div class="circle">
+							<i class="fa fa-camera">&nbsp;</i>
+						</div>
+						<p>
+							<a href="#">Browse Our Photo Gallery</a><br />
+							Become familiar with the Talbott Recovery Campus<br /><br />
+							<a href="#">Browse Our Photo Gallery</a><br />
+							Learn about our Professionals Program, Recognizing Alcoholism and more.<br /><br />
+							<a href="#">Browse Our Photo Gallery</a><br />
+							Tour our Atlanta, Dunwoody, and Columbus drug and alcohol rehabilitation centers.<br /><br />
+						</p>
 					</div>
-					<p>
-						<a href="#">Browse Our Photo Gallery</a><br />
-						Become familiar with the Talbott Recovery Campus<br /><br />
-						<a href="#">Browse Our Photo Gallery</a><br />
-						Learn about our Professionals Program, Recognizing Alcoholism and more.<br /><br />
-						<a href="#">Browse Our Photo Gallery</a><br />
-						Tour our Atlanta, Dunwoody, and Columbus drug and alcohol rehabilitation centers.<br /><br />
-					</p>
 				</div>
 			</div>
 		</div>
